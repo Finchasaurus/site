b7bb8e7cdc550533c970da8976330ff28b778d14
Finch <finchasaurus@gmail.com>
2026-04-23 17:27:23 -0600

    hover color

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,15 +1,12 @@
 const button = document.querySelector(".carousel-filtering-button");
 const carousel = document.querySelector(".carousel");
-const items = document.querySelectorAll(".carousel ul");
 
 button.addEventListener("click", () => {
-	items.forEach((item) => {
-		const current = item.getAttribute("accent");
+	const current = carousel.getAttribute("accent");
 
-		if (current === "hover-clear tint" || current === "tint hover-clear") {
-			item.removeAttribute("accent");
-		} else {
-			item.setAttribute("accent", "hover-clear tint");
-		}
-	});
+	if (current === "hover-clear tint" || current === "tint hover-clear") {
+		carousel.removeAttribute("accent");
+	} else {
+		carousel.setAttribute("accent", "hover-clear tint");
+	}
 });
